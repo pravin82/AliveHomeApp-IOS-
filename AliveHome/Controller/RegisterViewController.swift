42fd63c45848703abd211bfaf65a1b434ebe27df
pravinmishra <pravinmishra1796@gmail.com>
2018-05-09 16:30:53 +0530

    Implement the function for checking Username and EmailId

--- AliveHome/Controller/RegisterViewController.swift
+++ AliveHome/Controller/RegisterViewController.swift
@@ -12,12 +12,25 @@ class RegisterViewController: UIViewController {
 
     @IBOutlet weak var selectSecurityOutlet: UIButton!
     @IBOutlet var securityQuestionOutlet: [UIButton]!
+    
+    @IBOutlet weak var hardwareIdTextField: UITextField!
+    
+    @IBOutlet weak var providedUsernameTextField: UITextField!
+    
+    @IBOutlet weak var providedPasswordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailIdTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var securityAnswerTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
        securityQuestionOutlet.forEach { (securityQuestion) in
            securityQuestion.isHidden=true
         }
         // Do any additional setup after loading the view.
+     
+        
     }
     
     
@@ -27,6 +40,40 @@ class RegisterViewController: UIViewController {
           securityQuestion.isHidden = !securityQuestion.isHidden
         }
     }
+    @IBAction func registerButton(_ sender: UIButton) {
+        let hardwareId=hardwareIdTextField.text;
+        let providedUsername=providedUsernameTextField.text;
+        let providedPassword=providedPasswordTextField.text;
+        let username=usernameTextField.text;
+        let emailId=emailIdTextField.text;
+        let password=passwordTextField.text;
+        let confirmPassword=confirmPasswordTextField.text;
+        let securityAnswer=securityAnswerTextField.text;
+        if   isValidUsername(Input: username!)
+        {
+            print("valid")
+        }
+        else{
+            print("Invalid")
+        }
+        
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    func isValidUsername(Input:String) -> Bool {
+        let RegEx = "\\A\\w{6,18}\\z"
+        let Test = NSPredicate(format:"SELF MATCHES %@", RegEx)
+        return Test.evaluate(with: Input)
+    }
+   
+    
+    
+   
     
 
     @IBAction func securityQuestionButton(_ sender: UIButton) {
