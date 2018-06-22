c38f08f87d7cedd7fe2eefa51b4d0324d9476d72
pravinmishra <pravinmishra1796@gmail.com>
2018-06-22 23:23:11 +0530

    Fan Function completed

--- AliveHome/Controller/MainUIViewController.swift
+++ AliveHome/Controller/MainUIViewController.swift
@@ -36,41 +36,79 @@ class MainUIViewController: UIViewController,DevStateDelegate {
     @IBOutlet weak var fan_button: UIButton!
     
     @IBAction func fan_speed(_ sender: UIButton) {
+        if sender.tag==1{
+            fanState="FAN_ON_1"
+            fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan1"), for: .normal)
+            button_speed.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            button_speed1.backgroundColor=hexStringToUIColor(hex: "00fff3")
+            button_speed2.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            button_speed3.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            button_speed4.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            
+            sendToServer()
+        }
+        else if sender.tag==2{
+            fanState="FAN_ON_2"
+            fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan2"), for: .normal)
+            button_speed.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+            button_speed2.backgroundColor=hexStringToUIColor(hex: "#00bbd1")
+            button_speed3.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            button_speed4.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            sendToServer()
+        }
+        else if sender.tag==3{
+            fanState="FAN_ON_3"
+            fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan3"), for: .normal)
+            button_speed.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+            button_speed2.backgroundColor=hexStringToUIColor(hex: "#00bbd1")
+            button_speed3.backgroundColor=hexStringToUIColor(hex: "#007dff")
+            button_speed4.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            sendToServer()
+        }
+        else if sender.tag==4{
+            fanState="FAN_ON_4"
+            fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan4"), for: .normal)
+            button_speed.backgroundColor=hexStringToUIColor(hex: "#EBEBF1")
+            button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+            button_speed2.backgroundColor=hexStringToUIColor(hex: "#00bbd1")
+            button_speed3.backgroundColor=hexStringToUIColor(hex: "#007dff")
+            button_speed4.backgroundColor=hexStringToUIColor(hex: "#1000ff")
+            sendToServer()
+        }
+        else if sender.tag==5{
+            fanState=="FAN_ON_5"
+            fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan5"), for: .normal)
+            button_speed.backgroundColor=hexStringToUIColor(hex: "#0200b9")
+            button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+            button_speed2.backgroundColor=hexStringToUIColor(hex: "#00bbd1")
+            button_speed3.backgroundColor=hexStringToUIColor(hex: "#007dff")
+            button_speed4.backgroundColor=hexStringToUIColor(hex: "#1000ff")
+            sendToServer()
+        }
     }
     @IBAction func button_fan(_ sender: Any) {
-        
-        var array = [UInt8](repeating: 0x00, count: 16)
-        let iv=array.toBase64()
-        var digest = SHA2(variant: .sha256)
-        
-        let array1: [UInt8] = Array(sharedAesKey!.utf8)
-        print("sharedAesKey Main UI :"+sharedAesKey! )
         
         if fanState=="FAN_OFF"{
             fanState="FAN_ON_1"
             fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan1"), for: .normal)
+            button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+            
             sendToServer()
             
-            
-        }
+            }
         else{  fanState="FAN_OFF"
             fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan"), for: .normal)
-            let str1="CTRL-" + username!
-            let str2="-" + bulbState
-            let str3="-" + fanState
-            let str4="-" + transfer_session!
-            let str5=str1+str2+str3+str4
-            do{
-                let partial1 = try digest.update(withBytes:array1 )
-                let result=try digest.finish()
-                print ("str5  :"+str5)
-                //  let sm1="sessionRequest-"+username!
-                let esm1=try str5.aesEncrypt(key: result, iv: array)
-                logInVC.socket.write(string: esm1)
-            }
-            catch{}
-        }
+            button_speed1.backgroundColor = hexStringToUIColor(hex: "EBEBF1")
+            button_speed2.backgroundColor = hexStringToUIColor(hex: "EBEBF1")
+            button_speed3.backgroundColor = hexStringToUIColor(hex: "EBEBF1")
+            button_speed4.backgroundColor = hexStringToUIColor(hex: "EBEBF1")
+            button_speed.backgroundColor = hexStringToUIColor(hex: "EBEBF1")
             
+            sendToServer()
+        }
+        
         
         
     }
@@ -94,71 +132,56 @@ class MainUIViewController: UIViewController,DevStateDelegate {
         }
         else if state=="FAN_ON_5"{
             fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan1"), for: .normal)
+            button_speed.backgroundColor=hexStringToUIColor(hex: "#0200b9")
+            button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+            button_speed2.backgroundColor=hexStringToUIColor(hex: "#00bbd1")
+            button_speed3.backgroundColor=hexStringToUIColor(hex: "#007dff")
+            button_speed4.backgroundColor=hexStringToUIColor(hex: "#1000ff")
         }
         else if state=="FAN_ON_1"{
             fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan2"), for: .normal)
+             button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+            
         }
         else if state=="FAN_oN_2"{
             fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan3"), for: .normal)
+             button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+             button_speed2.backgroundColor=hexStringToUIColor(hex: "#00bbd1")
         }
         else if state=="FAN_ON_3"{
             fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan4"), for: .normal)
+            button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+            button_speed2.backgroundColor=hexStringToUIColor(hex: "#00bbd1")
+            button_speed3.backgroundColor=hexStringToUIColor(hex: "#007dff")
         }
         else if state=="FAN_ON_4"{
             fan_button.setImage(#imageLiteral(resourceName: "ic_home_fan5"), for: .normal)
+            button_speed1.backgroundColor=hexStringToUIColor(hex: "#00fff3")
+            button_speed2.backgroundColor=hexStringToUIColor(hex: "#00bbd1")
+            button_speed3.backgroundColor=hexStringToUIColor(hex: "#007dff")
+            button_speed4.backgroundColor=hexStringToUIColor(hex: "#1000ff")
         }
     }
     
+    @IBOutlet weak var button_speed1: UIButton!
+    @IBOutlet weak var button_speed2: UIButton!
+    @IBOutlet weak var button_speed3: UIButton!
+    @IBOutlet weak var button_speed: UIButton!
+    @IBOutlet weak var button_speed4: UIButton!
     @IBAction func button_light(_ sender: Any) {
-      //  let  wsuri = "ws://alivehome.iitkgp.ac.in:81"
-       // socket = WebSocket(url: URL(string: wsuri)!)
-        //socket.connect()
-        
-        
-        
-        var array = [UInt8](repeating: 0x00, count: 16)
-        let iv=array.toBase64()
-        var digest = SHA2(variant: .sha256)
-        
-        let array1: [UInt8] = Array(sharedAesKey!.utf8)
-        print("sharedAesKey Main UI :"+sharedAesKey! )
-       
+      
         if bulbState=="TL_ON"{
             bulbState="TL_OFF"
             button_light.setImage(#imageLiteral(resourceName: "bulb_off"), for: .normal)
-            let str1="CTRL-" + username!
-            let str2="-" + bulbState
-            let str3="-" + fanState
-            let str4="-" + transfer_session!
-            let str5=str1+str2+str3+str4
-            do{
-            let partial1 = try digest.update(withBytes:array1 )
-            let result=try digest.finish()
-                print ("str5  :"+str5)
-              //  let sm1="sessionRequest-"+username!
-             let esm1=try str5.aesEncrypt(key: result, iv: array)
-                logInVC.socket.write(string: esm1)
-            }
-            catch{}
+            sendToServer()
+            
             
             }
         else{
             button_light.setImage(#imageLiteral(resourceName: "bulb_on"), for: .normal)
             bulbState = "TL_ON"
-            let str1="CTRL-" + username!
-            let str2="-" + bulbState
-            let str3="-" + fanState
-            let str4="-" + transfer_session!
-            let str5=str1+str2+str3+str4
-            do{
-                let partial1 = try digest.update(withBytes:array1 )
-                let result=try digest.finish()
-                print ("str5  :"+str5)
-                //  let sm1="sessionRequest-"+username!
-                let esm1=try str5.aesEncrypt(key: result, iv: array)
-                logInVC.socket.write(string: esm1)
-            }
-            catch{}
+            sendToServer()
+            
         }
         
         
@@ -202,7 +225,27 @@ class MainUIViewController: UIViewController,DevStateDelegate {
         }
         catch{}
     }
-    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
     
 
    
